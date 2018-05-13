23c6149cb0d28d04ef6aa56f08e4eb7e8dcd7c23
Ketan Bhatt <ketanbhatt1006@gmail.com>
2018-05-14 02:11:47 +0530

    [#3] Enable hiding messages based on new UI changes

--- README.md
+++ README.md
@@ -15,7 +15,11 @@ Install Happy Slack! from [Chrome Web Store](https://chrome.google.com/webstore/
 ## How To Use:
 1. Click on the extension icon or navigate to extension options.
 2. Enter the `ID` of the user/bot you want to block. **NOTE:** This ID is not the slack handle the user/bot has, but the unique ID that slack assigns to every User/Bot. 
-3. You can get the ID by **right clicking on the user's/bot's name/photo from any channel**, and **selecting `Inspect`**. From there, look for the property **`data-member-id` (if it is a user or slackbot) OR `data-bot-id` (if it is any other bot)**. That is the ID we are looking for.
+3. You can get the ID by:
+    1. Click on the user's/bot's name/photo from any channel
+    2. Select **View Profile**
+    3. The user/bot's profile will open in the side bar on the right hand side. Click on the **downwards arrow** icon besides **Message**.
+    4. Select **Copy Member ID**
 4. You can block multiple users or bots by specifying their User IDs separated by `,`.
 5. Make sure you want to make Slack a happy place again.
 6. Save!
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -12,24 +12,39 @@ chrome.extension.sendMessage({}, function(response) {
 			return channel_title_div == undefined  // DMs do not have a DOM element with this ID
 		}
 
-		var should_hide_message = function(message){
-			var senderId = message.getAttribute('data-member-id')
-			if (senderId == ""){
-				senderId = message.getAttribute('data-bot-id')	
+		var get_sender_id = function(message, is_thread=false){
+			var senderId = undefined
+
+			if (is_thread) {
+				senderId = message.getAttribute('data-member-id')
+				if (senderId == ""){
+					senderId = message.getAttribute('data-bot-id')	
+				}
+			} else {
+				senderId = message.getElementsByClassName('c-message__avatar')[0]
+				if (senderId != undefined) {
+					senderId = senderId.href.split('/').pop()
+				}
 			}
 
+			return senderId
+		}
+		var should_hide_message = function(message, is_thread){
+			senderId = get_sender_id(message, is_thread)
 			return Boolean(blockedUsers.indexOf(senderId) > -1)
 		}
 		var hide_message = function(message){
 			message.style.display = "none";
 		}
-		var handle_message = function(message){
-			if (should_hide_message(message)){
+
+		var handle_message = function(message, is_thread){
+			if (should_hide_message(message, is_thread)){
 				hide_message(message)
 			}
 		}
 
-		message_div = $('#msgs_div')  // Parent div that contains messages
+		message_div = $('#messages_container')  // Parent div that contains main messages
+		thread_div = $('#flex_contents')  // Parent div that contains threads in sidebar
 
 		chrome.storage.sync.get({
 			blockedUsers: "",
@@ -44,7 +59,7 @@ chrome.extension.sendMessage({}, function(response) {
 				return
 			}
 
-			// Add function to be called everytime a new node is inserted in message_div
+			// Add function to Remove messages from the main window
 			message_div.bind('DOMNodeInserted', function(event){
 				if (onlyBlockDMs && !is_dm_window()) {
 					return
@@ -53,19 +68,45 @@ chrome.extension.sendMessage({}, function(response) {
 				event_target = event.target;
 
 				// Handle new incoming messages
-				if (event_target.tagName == "TS-MESSAGE"){
-					handle_message(event_target)
+				if (event_target.className == "c-virtual_list__item"){
+					handle_message(event_target, false)
 				} 
 
-				// Handle message history loading
+			})
+
+			// Add function to Remove messages from threads
+			thread_div.bind('DOMNodeInserted', function(event){
+				event_target = event.target;
+
+				console.log("DEBUG:: THREAD", event, event_target.tagName)
+
+				// Handle new incoming thread messages
+				if (event_target.tagName == "TS-MESSAGE"){
+					handle_message(event_target, true)
+				} 
+
+				// Handle Thread history loading
 				else if (event_target.className == 'day_container'){
 					messages = event_target.getElementsByTagName('ts-message')
 					for (i=0; i<messages.length; i++){
-						handle_message(messages[i])
+						handle_message(messages[i], true)
 					}
 				}
 
 			})
+
+			// Handle main messages' history
+			all_messages = $('.c-virtual_list__item')
+			for (i=0; i<all_messages.length; i++){
+				handle_message(all_messages[i], false)
+			}
+
+			// Handle thread's history
+			messages = document.getElementsByTagName('ts-message')
+			for (i=0; i<messages.length; i++){
+				handle_message(messages[i], true)
+			}
+
 		});
 	}
 	}, 10);
